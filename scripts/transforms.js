c94c63d3ed9010eae93f87b64239490b0fdc41c7
Jack Reichenbach <jreichen@gustavus.edu>
2020-04-13 21:03:56 -0500

    Update transforms.js
    render

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -3,30 +3,60 @@ function Mat4x4Parallel(mat4x4, prp, srp, vup, clip) {
     var transform = [];
     transform[5] = mat4x4;
     // 1. translate PRP to origin
-    transform[4] = Mat4x4Translate(mat4x4, -prp.x, -prp.y, -prp.z);
+    transform[4] = new Matrix(4, 4);
+    Mat4x4Translate(transform[4], -prp.x, -prp.y, -prp.z);
+
     // 2. rotate VRC such that (u,v,n) align with (x,y,z)
-    var n = (prp - srp).normalize;
-    var u = (vup - n).normalize;
-    var v = n.mult(v);
-    var vrc = (n = n, u = u, v = v);
-    var rotateMatrix = [[vrc.u[0], vrc.u[1], vrc.u[2], 0],
-                        [vrc.v[0], vrc.v[1], vrc.v[2], 0],
-                        [vrc.n[0], vrc.n[1], vrc.n[2], 0],
-                        [       0,        0,        0, 1]];
+
+    var n = Vector3(prp.x-srp.x, prp.y-srp.y, prp.z-srp.z);
+    n.normalize();
+
+    var temp = vup.cross(n);
+    var u = Vector3(temp.x, temp.y, temp.z);
+    u.normalize();
+    
+    var v = u.cross(n);
+    
+    var vrc = [n, u, v];
+
+    var temp = [[vrc[1].x, vrc[1].y, vrc[1].z, 0],
+                [vrc[2].x, vrc[2].y, vrc[2].z, 0],
+                [vrc[0].x, vrc[0].y, vrc[0].z, 0],
+                [       0,        0,        0, 1]];
+
+    var rotateMatrix = new Matrix(4, 4);
+    rotateMatrix.values = temp;
     transform[3] = rotateMatrix;
+
     // 3. shear such that CW is on the z-axis
     var cw = (((clip[2] + clip[3])/2),((clip[0] + clip[1])/2));
     var dop = cw - prp;
-    transform[2] = Mat4x4ShearXY(mat4x4, (-dop.x / dop.z), dop.y / dop.z);
-    // 4. translate near clipping plane to origin
-    transform[1] = Mat4x4Translate(mat4x4, tx, ty, clip[4]);
+    transform[2] = new Matrix(4, 4);
+    
+    Mat4x4ShearXY(transform[2], (-dop.x / dop.z), dop.y / dop.z);
+
+    // 4. translate near clipping plane to 
+    transform[1] = new Matrix(4, 4);
+    Mat4x4Translate(transform[1], 0, 0, -clip[4]);
+
     // 5. scale such that view volume bounds are ([-1,1], [-1,1], [-1,0])
     var sparx = 2/(clip[1] -clip[0]);
     var spary = 2/(clip[3] -clip[2]);
     var sparz = 1/(clip[4] -clip[5]);
-    transform[0] = mat4x4Scale(mat4x4, sparx, spary, sparz);
+
+    transform[0] = new Matrix(4, 4);
+    Mat4x4Scale(transform[0], sparx, spary, sparz);
+    
     transformation = Matrix.multiply(transform);
     mat4x4.values = transformation.values;
+
+    
+    var mat1 = new Matrix(4, 4);
+    Mat4x4MPar(mat1);
+    mat = new Matrix(4, 4);
+    mat = Matrix.multiply([mat1, transformation]);
+    transformation = mat;
+
 }
 
 // set values of mat4x4 to the perspective projection / view matrix
@@ -39,7 +69,7 @@ function Mat4x4Projection(mat4x4, prp, srp, vup, clip) {
     var n = (prp - srp).normalize;
     var u = (vup - n).normalize;
     var v = n.mult(v);
-    var vrc = (n = n, u = u, v = v);
+    var vrc = (n, u, v);
     var rotateMatrix = [[vrc.u[0], vrc.u[1], vrc.u[2], 0],
                         [vrc.v[0], vrc.v[1], vrc.v[2], 0],
                         [vrc.n[0], vrc.n[1], vrc.n[2], 0],
@@ -151,3 +181,4 @@ function Vector4(x, y, z, w) {
     vec4.values = [x, y, z, w];
     return vec4;
 }
+
